1101f5c8560863ef304985685fd3d302e067750e
Kaus <abc@gmail.com>
2021-08-18 20:33:26 +0530

    Initial Commit

---
+++ KhatabookTests/KhatabookTests.swift
@@ -0,0 +1,33 @@
+//
+//  KhatabookTests.swift
+//  KhatabookTests
+//
+//  Created by GCO on 18/08/21.
+//
+
+import XCTest
+@testable import Khatabook
+
+class KhatabookTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
